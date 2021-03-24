ea5971705baf4d4ccf03f98cbbd2b351dddde890
bborumoore <bborumoore@gmail.com>
2021-03-24 16:27:55 -0400

    Transfered over some basic work that I had done, set up file structure

---
+++ index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is your Project Title?', 
+    'What would you like displayed as the Description for your project?',
+    'What are the Installation Instructions for your project?',
+    'What is the usage information for your project?',
+    'What are the contribution guidelines for your project?',
+    'What are the test instructions for your project?',
+    'What license would you like for your project?',
+    'What is your GitHub username?',
+    'What is your email address?'
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
